89ae7390f08abc8347b99de4d4e4dab4b99de6cc
Viktoria <zaharova181114@gmail.com>
2021-02-11 21:59:30 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,8 @@
+$('.history-slider').slick({
+    slidesToShow: 3,
+    dots: true,
+    appendArrows: '.history-slider__nav',
+    appendDots: '.history-slider__nav',
+    centerMode: true,
+    variableWidth: true
+});
